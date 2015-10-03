9bfd16dc32b684e3dcbe79210649f98224b8d55a
Jose Pefaur <jose.pefaur@gmail.com>
2015-10-03 12:51:05 -0300

    cliente solamente salta y dibuja la GUI

--- src/cl/uchile/dcc/cc5303/Client.java
+++ src/cl/uchile/dcc/cc5303/Client.java
@@ -1,22 +1,48 @@
 package cl.uchile.dcc.cc5303;
 
-import java.awt.event.KeyEvent;
+import cl.uchile.dcc.cc5303.interfaces.IGame;
+import cl.uchile.dcc.cc5303.interfaces.IPlayer;
+import cl.uchile.dcc.cc5303.interfaces.IServer;
+
+import javax.swing.*;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-/**
- * Created by jose on 10/2/15.
- */
 public class Client {
 
     static public void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
     //    boolean[] keys;
     //    keys = new boolean[KeyEvent.KEY_LAST];
 
-        IGameEngine gameEngine = (IGameEngine) Naming.lookup(Server.url);
+        IServer gameEngine = (IServer) Naming.lookup(GameEngine.url);
         IPlayer player  = gameEngine.joinGame();
+        IGame game = gameEngine.getGame();
         player.startJumping();
+
+        Renderer renderer = new Renderer(800, 600);//width y height del game engine
+
+
+        JFrame frame = new JFrame("CLIENT");
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(renderer);
+
+        renderer.setSize(800, 600);
+
+        while(true) {
+            renderer.players = game.getPlayers();
+            System.out.println(renderer.players.size());
+            renderer.levels = game.getLevels();
+            System.out.println(renderer.levels.size());
+            renderer.repaint();
+            try {
+                Thread.sleep(1000 / 60);//UPDATE RATE DEL game engine
+            } catch (InterruptedException ex) {
+
+            }
+
+        }
     }
 }
