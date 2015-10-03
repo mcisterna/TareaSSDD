e54b9864f4a6c889af4794735966aaf1d77a90d8
Jose Pefaur <jpefaur@Joses-MacBook-Pro.local>
2015-10-03 00:47:01 -0300

    Jump sin loop

--- src/cl/uchile/dcc/cc5303/Client.java
+++ src/cl/uchile/dcc/cc5303/Client.java
@@ -17,9 +17,6 @@ public class Client {
 
         IGameEngine gameEngine = (IGameEngine) Naming.lookup(Server.url);
         IPlayer player  = gameEngine.joinGame();
-
-        while(true) {
-                player.startJumping();
-        }
+        player.startJumping();
     }
 }
