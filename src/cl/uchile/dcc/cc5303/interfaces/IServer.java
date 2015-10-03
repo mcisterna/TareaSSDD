1f13787ffd0c3987dfe0dfd4afedcaa83ee705a2
Jose Pefaur <jpefaur@Joses-MacBook-Pro.local>
2015-10-03 12:46:00 -0300

    sacar comentarios

--- src/cl/uchile/dcc/cc5303/interfaces/IServer.java
+++ src/cl/uchile/dcc/cc5303/interfaces/IServer.java
@@ -1,14 +1,8 @@
 package cl.uchile.dcc.cc5303.interfaces;
 
-import cl.uchile.dcc.cc5303.interfaces.IGame;
-import cl.uchile.dcc.cc5303.interfaces.IPlayer;
-
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * Created by jose on 10/2/15.
- */
 public interface IServer extends Remote{
 
     public IPlayer joinGame() throws RemoteException;
