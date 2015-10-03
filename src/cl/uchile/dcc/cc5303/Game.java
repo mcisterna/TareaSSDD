81539a54064eff823c486043875cb6f9384eb1e7
Jose Pefaur <jose.pefaur@gmail.com>
2015-10-03 12:52:03 -0300

    clase que contiene los elementos del juego

--- src/cl/uchile/dcc/cc5303/Game.java
+++ src/cl/uchile/dcc/cc5303/Game.java
@@ -1,70 +1,40 @@
 package cl.uchile.dcc.cc5303;
 
+import cl.uchile.dcc.cc5303.elements.Level;
+import cl.uchile.dcc.cc5303.elements.Player;
+import cl.uchile.dcc.cc5303.interfaces.IGame;
+import cl.uchile.dcc.cc5303.interfaces.IPlayer;
+
 import java.awt.*;
 import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Created by sebablasko on 9/11/15.
- */
-public class Game extends Canvas implements IBoard{
-
-    public int width, height;
+public class Game extends UnicastRemoteObject implements IGame {
 
     public List<IPlayer> players;
     public LinkedList<Level> levels;
-    public Color[] playerColors = {Color.red, Color.blue, Color.green, Color.cyan};
-    public Image img;
-    public Graphics buffer;
 
-    public Game(int w, int h, List players){
-        this.width = w;
-        this.height = h;
-        this.players = players;
-    }
-
-    @Override
-    public void update(Graphics g) { paint(g); }
-
-    @Override
-    public void paint(Graphics g){
-        if(buffer==null){
-            img = createImage(getWidth(),getHeight() );
-            buffer = img.getGraphics();
+    public Game(List<Player> players) throws RemoteException {
+        super();
+        this.players = new LinkedList<>();
+        for(IPlayer player : players) {
+            this.players.add(player);
         }
-
-        buffer.setColor(Color.black);
-        buffer.fillRect(0, 0, getWidth(), getHeight());;
-
-        for(int i = 0; i<players.size(); i++) {
-
-            buffer.setColor(playerColors[i]);
-            try {
-                players.get(i).draw(buffer);
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        buffer.setColor(Color.white);
-        for(Level l : levels){
-            l.draw(buffer);
-        }
-
-        g.drawImage(img, 0, 0, null);
     }
 
     public void levelsDown() {
         levels.add(new Level(2));
-        for(Level l : levels) {
-        	l.moveDown();
-        }
+        for(Level l : levels) l.moveDown();
         levels.remove();
     }
 
+    public List<IPlayer> getPlayers() throws RemoteException{
+        return players;
+    }
 
-
-
+    public List<Level> getLevels() throws RemoteException{
+        return levels;
+    }
 }
