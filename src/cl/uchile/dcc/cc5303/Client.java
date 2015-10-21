37f298aea2071ddb7292aa5795616ad8169cb9a4
Jose Pefaur <jose.pefaur@gmail.com>
2015-10-21 13:43:18 -0300

    Servidor espera a que todos los clientes lean el ranking

--- src/cl/uchile/dcc/cc5303/Client.java
+++ src/cl/uchile/dcc/cc5303/Client.java
@@ -6,7 +6,6 @@ import cl.uchile.dcc.cc5303.interfaces.IServer;
 
 import javax.swing.*;
 
-import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.net.MalformedURLException;
@@ -16,98 +15,103 @@ import java.rmi.RemoteException;
 
 public class Client {
 
-    static public void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
-    	System.out.println("Starting client...");
-        final boolean[] keys = new boolean[KeyEvent.KEY_LAST];
-        if(args.length == 0){
-        	System.out.println("Debe ingresar ip");
-        	return;
-        }
-        String ip = args[0];
-    	String url = "rmi://"+ip+":1099/game";
-        IServer server = (IServer) Naming.lookup(url);
-        IPlayer player  = server.joinGame();
-        Color color = player.getColor();
+	private final static String TITLE = "Juego - CC5303";
+	private final static int WIDTH = 800, HEIGHT = 600;
 
-        if(player == null) {
-            System.out.println("Game is full");
-        }
-        else {
-            IGame game = server.getGame();
+	static public void main(String[] args) throws RemoteException, NotBoundException, MalformedURLException {
+		System.out.println("Starting client...");
+		final boolean[] keys = new boolean[KeyEvent.KEY_LAST];
+		if (args.length == 0) {
+			System.out.println("Debe ingresar ip");
+			return;
+		}
+		String ip = args[0];
+		String url = "rmi://" + ip + ":1099/game";
+		IServer server = (IServer) Naming.lookup(url);
+		IPlayer player = server.joinGame();
 
-            JFrame frame = new JFrame("CLIENT");
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            Renderer renderer = new Renderer(800, 600);//width y height del game engine
-            renderer.setSize(800, 600);
+		if (player == null) {
+			System.out.println("Game is full");
+		} else {
+			IGame game = server.getGame();
 
-            frame.add(renderer);
-            frame.pack();
-            frame.addKeyListener(new KeyListener() {
-                @Override
-                public void keyTyped(KeyEvent e) {
-                }
+			JFrame frame = new JFrame(TITLE);
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Renderer renderer = new Renderer(WIDTH, HEIGHT);//width y height del game engine
+			renderer.setSize(WIDTH, HEIGHT);
 
-                @Override
-                public void keyPressed(KeyEvent e) {
-                    keys[e.getKeyCode()] = true;
-                }
+			frame.add(renderer);
+			frame.pack();
+			frame.addKeyListener(new KeyListener() {
+				@Override
+				public void keyTyped(KeyEvent e) {
+				}
 
-                @Override
-                public void keyReleased(KeyEvent e) {
-                    keys[e.getKeyCode()] = false;
-                }
-            });
+				@Override
+				public void keyPressed(KeyEvent e) {
+					keys[e.getKeyCode()] = true;
+				}
 
-            while(true){
-            	renderer.playing = true;
-	            while (game.getPlayers().size()>0) {
-	
-	                if (keys[KeyEvent.VK_UP]) {
-	                    player.startJumping();
-	                }
-	                else {
-	                    player.stopJumping();
-	                }
-	                if (keys[KeyEvent.VK_RIGHT]) {
-	                    player.startMovingRight();
-	                }
-	                else {
-	                    player.stopMovingRight();
-	                }
-	                if (keys[KeyEvent.VK_LEFT]) {
-	                    player.startMovingLeft();
-	                }
-	                else {
-	                    player.stopMovingLeft();
-	                }
-	                renderer.players = game.getPlayers();
-	                renderer.levels = game.getLevels();
-	                renderer.repaint();
-	                try {
-	                    Thread.sleep(1000 / 60);//UPDATE RATE DEL game engine
-	                } catch (InterruptedException ex) {
-	
-	                }
-	
-	                frame.requestFocus();
-	            }
-	            renderer.playing = false;
-	            renderer.ranking = game.getRanking();
-	            
-	            while(true){
-	            	renderer.repaint();
-	            	 try {
-		                    Thread.sleep(1000 / 60);
-		                } catch (InterruptedException ex) {
-		
-		                }
-	            	 frame.requestFocus();
-	            }
+				@Override
+				public void keyReleased(KeyEvent e) {
+					keys[e.getKeyCode()] = false;
+				}
+			});
 
-	            
-            }
-            
-        }
-    }
+			while (true) {
+				while (game.getPlayers().size() > 0) {
+
+					if (keys[KeyEvent.VK_UP]) {
+						player.startJumping();
+					} else {
+						player.stopJumping();
+					}
+					if (keys[KeyEvent.VK_RIGHT]) {
+						player.startMovingRight();
+					} else {
+						player.stopMovingRight();
+					}
+					if (keys[KeyEvent.VK_LEFT]) {
+						player.startMovingLeft();
+					} else {
+						player.stopMovingLeft();
+					}
+					renderer.players = game.getPlayers();
+					renderer.levels = game.getLevels();
+					renderer.repaint();
+					try {
+						Thread.sleep(1000 / 60);//UPDATE RATE DEL game engine
+					} catch (InterruptedException ex) {
+
+					}
+
+					frame.requestFocus();
+				}
+				renderer.playing = false;
+				renderer.ranking = game.getRanking();
+				server.playerIsReadyToContinue();
+
+				while (!renderer.playing) {
+					renderer.repaint();
+					try {
+						Thread.sleep(1000 / 60);
+					} catch (InterruptedException ex) {
+
+					}
+					frame.requestFocus();
+
+					if (keys[KeyEvent.VK_ENTER]) {
+						renderer.playing = true;
+					}
+				}
+
+				game = server.getGame();
+				server.joinGame();
+
+			}
+
+		}
+	}
 }
+
