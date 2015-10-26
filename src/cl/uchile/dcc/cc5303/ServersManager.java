203f98b453159f0491dbd1a7703f40bf8a65937a
Jose Pefaur <jose.pefaur@gmail.com>
2015-10-26 10:20:22 -0300

    implementacion servidor que administra al resto

--- src/cl/uchile/dcc/cc5303/ServersManager.java
+++ src/cl/uchile/dcc/cc5303/ServersManager.java
@@ -1,7 +1,34 @@
 package cl.uchile.dcc.cc5303;
 
+import cl.uchile.dcc.cc5303.interfaces.IServer;
+
+import java.util.ArrayList;
+
 /**
  * Created by jose on 10/21/15.
  */
 public class ServersManager {
+
+    private ArrayList<IServer> servers;
+
+    private ServersManager() {
+        this.servers = new ArrayList<>();
+    }
+
+    public void addServer(IServer server) {
+        servers.add(server);
+    }
+
+    public IServer getLowestLoadServer() {
+        IServer lowestLoadServer = null;
+        double lowestLoad = 1;
+
+        for(IServer server : servers) {
+            if(server.getLoad() < lowestLoad) lowestLoadServer = server;
+        }
+
+        return  lowestLoadServer;
+    }
+
+
 }
